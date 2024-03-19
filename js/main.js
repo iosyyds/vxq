7aef071288cf9c8e1e18a8e81a86a9a789fc6644
甜甜 <145662294+iosyyds@users.noreply.github.com>
2024-03-19 11:29:07 +0800

    群更新
    Signed-off-by: 甜甜 <145662294+iosyyds@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	var qrCode3Image = new Image();
 	qrCode3Image.src = new QRCode(document.createElement("div"), {
-		text: "https://qm.qq.com/q/doe3JOrJRK",
+		text: "https://qm.qq.com/q/c2yIcuV8AM",
 		width: 300,
 		height: 300,
 		correctLevel: QRCode.CorrectLevel.H,
